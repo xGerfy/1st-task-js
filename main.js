06b9b5a79cc8faac3bf3a9916c1b06508c572ae9
Roman Ponomarev <roman.ponomarev66@gmail.com>
2021-12-10 02:32:49 +0400

    Confirm

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+const overlay = document.querySelector('.overlay')
+
+const openModal = (src) => {
+  const modalImg = document.querySelector('.modal__img')
+  modalImg.src = src
+
+  overlay.classList.remove('hide')
+}
+
+const hideModal = (event) => {
+  console.log(event.target.className);
+  if (event.target.className === 'overlay') {
+    overlay.classList.add('hide')
+  }
+}
+ overlay.addEventListener("click", hideModal)
 
 async function fetchResponse() {
   let response = await fetch('https://react-learnwords-example.herokuapp.com/words')
@@ -6,96 +22,20 @@ async function fetchResponse() {
 
   let picturs = document.querySelector('.picturs')
 
-  let key;
-  for (key in content) {
-    picturs.innerHTML += `
-      <a href="#" class="btn__img" data-pict="pic"><img src="https://react-learnwords-example.herokuapp.com/${content[key].image}" alt="picture" class="${content[key].image}"></img></a>
-    ` 
+  for (let index = 0; index < content.length; index++) {
+    const imageSrc = `https://react-learnwords-example.herokuapp.com/${content[index].image}`
+
+    const imageBox = document.createElement('div')
+    imageBox.className= "btn__img"
+    imageBox.addEventListener("click", () => openModal(imageSrc))
+
+    const img = document.createElement('img')
+    img.alt = "picture"
+    img.src = `https://react-learnwords-example.herokuapp.com/${content[index].image}`
+
+    imageBox.append(img)
+    picturs.append(imageBox)
   }
 }
 
 fetchResponse()
-
-
-
-function _createModal (options) {
-  const modal = document.createElement('div')
-  modal.classList.add('rmodal')
-  modal.insertAdjacentHTML('afterbegin', `
-    <div class="modal__overlay" data-close="true">
-      <div class="modal__window">
-        <div class="modal__header">
-          ${options.closable ? `<span class="modal__close" data-close="true">&times;</span>` : ''}
-        </div>
-        <div class="modal__body">
-          ${options.content || ''}
-        </div>
-      </div>
-    </div>
-  `)
-  document.body.appendChild(modal)
-  // console.log($.modal);
-  return modal
-}
-
-$.modal = function(options) {
-  const ANIMATION__SPEED = 200
-  const $modal = _createModal(options)
-  let closing = false
-  let destroyed = false
-
-  const modal = {
-    open() {
-      if (destroyed) {
-        return console.log('modal is destroyed')
-      }
-      !closing && $modal.classList.add('open')
-    },
-    close() {
-      closing = true
-      $modal.classList.remove('open')
-      $modal.classList.add('hiden')
-      setTimeout(() => {
-        $modal.classList.remove('hiden')
-        closing = false
-      }, ANIMATION__SPEED)
-    },
-  }
-
-  const listiner = event => {
-    if (event.target.dataset.close) {
-      modal.close()
-    }
-  }
-  
-  $modal.addEventListener('click', listiner)
-
-  return Object.assign(modal, {
-    destroy() {
-      $modal.parentNode.removeChild($modal)
-      $modal.removeEventListener('click', listiner)
-      destroyed = true
-    }
-  })
-}
-
-
-
-// const modal = $.modal({
-//   closable: true,
-//   content: '',
-//   width: '600px'
-// })
-
-
-// document.addEventListener('click', event => {
-//   event.preventDefault()
-//   const dataimg = event.target.dataset.pict
-//   if (dataimg === 'pic') {
-//     console.log('pic');
-//     // modal.open
-//   }
-// })
-
-
-// $.modal()
